8ae4734e2a3ca20bec5782709419705f81810aea
Dmitry Bolshakov <dmitry-b@yandex.ru>
2023-01-02 20:21:35 +0300

    Added mongodb

---
+++ mongodb/mongoosemvc/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Movie = require('./models/movie');
+const movieRoutes = require('./routes/movie-routes');
+
+const PORT = 3000;
+const URL = 'mongodb://localhost:27017/moviebox';
+
+const app = express();
+
+app.use(express.json());
+app.use(movieRoutes);
+
+mongoose
+    .connect(URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(`DB connection error ${err}`));
+     
+app.listen(PORT, (err) => {
+    err ? console.log(err) : console.log(`Listening port ${PORT}`);
+});
+     
+
+
